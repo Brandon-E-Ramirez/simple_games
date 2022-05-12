10ea24dd77d5ee0d1d2aeb307fb8715877237189
Brandon E Ramirez <brandon.evaristo.ramirez@gmail.com>
2022-05-12 15:46:45 -0500

    added generic fix

--- rockPaperScissors/src/script.js
+++ rockPaperScissors/src/script.js
@@ -1,7 +1,7 @@
 let computerChoiceDisplay = document.getElementById('computer-choice')
 let userChoiceDisplay = document.getElementById('user-choice')
 let displayResult = document.getElementById('result') 
-
+ 
 //New values
 let compTot = document.getElementById('computer-wins')
 let usrTot = document.getElementById('user-wins')
